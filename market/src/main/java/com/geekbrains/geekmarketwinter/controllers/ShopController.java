6865f48e80155157af61a8e490eceed985f608d8
Mihail_Vorobyov <ptichkinm@mail.ru>
2021-11-18 19:26:52 +0300

    Доработан заказ (кроме оплаты)

--- market/src/main/java/com/geekbrains/geekmarketwinter/controllers/ShopController.java
+++ market/src/main/java/com/geekbrains/geekmarketwinter/controllers/ShopController.java
@@ -1,9 +1,10 @@
 package com.geekbrains.geekmarketwinter.controllers;
 
+import com.geekbrains.geekmarketwinter.entities.DeliveryAddress;
+import com.geekbrains.geekmarketwinter.entities.Order;
 import com.geekbrains.geekmarketwinter.entities.OrderItem;
 import com.geekbrains.geekmarketwinter.entities.User;
-import com.geekbrains.geekmarketwinter.services.ProductService;
-import com.geekbrains.geekmarketwinter.utils.Order;
+import com.geekbrains.geekmarketwinter.services.*;
 import com.geekbrains.geekmarketwinter.utils.ShoppingCart;
 import contract.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +12,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import contract.specifications.ProductSpecs;
-import com.geekbrains.geekmarketwinter.services.SendMessageService;
-import com.geekbrains.geekmarketwinter.services.ShoppingCartService;
-import com.geekbrains.geekmarketwinter.services.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,6 +39,12 @@ public class ShopController {
     
     @Autowired
     private SendMessageService sendMessageService;
+    
+    @Autowired
+    private OrderService orderService;
+    
+    @Autowired
+    private DeliveryAddressService deliveryAddressService;
 
     @GetMapping
     public String shopPage(Model model,
@@ -91,24 +94,71 @@ public class ShopController {
     }
 
     private final static String QUEUE_NAME = "orderQueue";
-
+    
     @GetMapping("/order/fill")
-    public String placeOrder(Model model, HttpServletRequest httpServletRequest) {
-        HttpSession httpSession = httpServletRequest.getSession();
-        String userName = httpServletRequest.getUserPrincipal().getName();
-        double totalCost = shoppingCartService.getTotalCost(httpSession);
-        String msg = String.format("New order created. User = %s, total cost = %.2f", userName, totalCost);
-
-        ShoppingCart cart = shoppingCartService.getCurrentCart(httpSession);
-        User user = (User) httpSession.getAttribute("user");
-        List<OrderItem> orderItems = cart.getItems();
-        double price = cart.getTotalCost();
-
-        Order order = new Order(user, orderItems, price);
+    public String orderFill(Model model, HttpServletRequest httpServletRequest, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        User user = userService.findByUserName(principal.getName());
+        Order order = orderService.makeOrder(shoppingCartService.getCurrentCart(httpServletRequest.getSession()), user);
+        List<DeliveryAddress> deliveryAddresses = deliveryAddressService.getUserAddresses(user.getId());
         model.addAttribute("order", order);
-
-        sendMessageService.sendMessage(QUEUE_NAME, msg);
-
+        model.addAttribute("deliveryAddresses", deliveryAddresses);
+        return "order-filler";
+    }
+    
+//    @GetMapping("/order/confirm")
+//    public String placeOrder(Model model, HttpServletRequest httpServletRequest) {
+//        HttpSession httpSession = httpServletRequest.getSession();
+//        String userName = httpServletRequest.getUserPrincipal().getName();
+//        double totalCost = shoppingCartService.getTotalCost(httpSession);
+//        String msg = String.format("New order created. User = %s, total cost = %.2f", userName, totalCost);
+//
+//        ShoppingCart cart = shoppingCartService.getCurrentCart(httpSession);
+//        User user = (User) httpSession.getAttribute("user");
+//        List<OrderItem> orderItems = cart.getItems();
+//        double price = cart.getTotalCost();
+//
+//        Order order = orderService.makeOrder(shoppingCartService.getCurrentCart(httpServletRequest.getSession()), user);
+//        model.addAttribute("order", order);
+//
+//        sendMessageService.sendMessage(QUEUE_NAME, msg);
+//
+//        return "order-result";
+//    }
+    
+    @PostMapping("/order/confirm")
+    public String orderConfirm(Model model, HttpServletRequest httpServletRequest, @ModelAttribute(name = "order") Order orderFromFrontend, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        User user = userService.findByUserName(principal.getName());
+        Order order = orderService.makeOrder(shoppingCartService.getCurrentCart(httpServletRequest.getSession()), user);
+        order.setDeliveryAddress(orderFromFrontend.getDeliveryAddress());
+        order.setPhoneNumber(orderFromFrontend.getPhoneNumber());
+        order.setDeliveryDate(LocalDateTime.now().plusDays(7));
+        order.setDeliveryPrice(0.0);
+        order = orderService.saveOrder(order);
+        model.addAttribute("order", order);
+        return "order-filler";
+    }
+    
+    @GetMapping("/order/result/{id}")
+    public String orderConfirm(Model model, @PathVariable(name = "id") Long id, Principal principal) {
+        if (principal == null) {
+            return "redirect:/login";
+        }
+        // todo ждем до оплаты, проверка безопасности и проблема с повторной отправкой письма сделать одноразовый вход
+        User user = userService.findByUserName(principal.getName());
+        Order confirmedOrder = orderService.findById(id);
+        if (!user.getId().equals(confirmedOrder.getUser().getId())) {
+            return "redirect:/";
+        }
+//        mailService.sendOrderMail(confirmedOrder);
+        model.addAttribute("order", confirmedOrder);
         return "order-result";
     }
+    
+    
 }
